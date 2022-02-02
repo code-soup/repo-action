610dfe8e832678d934fcb26dc8c671207dd79fed
Vlado Bosnjak <vb@bobz.co>
2022-02-02 11:50:49 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-# Test
-echo "1: $1"
-echo "2: $2"
-echo "3: ${INPUT_HOST}"
-echo "4: ${INPUT_SECRET}"
-echo "5: ${INPUTS_HOST}"
-echo "6: ${INPUTS_SECRET}"
-echo "7: ${HOST}"
-echo "8: ${MY_HOST}"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
