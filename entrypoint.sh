dfd2620b1f8fa0801dc176a7a73255d157dea381
Vlado Bosnjak <vb@bobz.co>
2022-03-19 17:59:50 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-echo "Ziploy ID $1"
-echo "Ziploy host $2"
-echo "Ziploy secret $3"
-
 # Download latest CLI
 curl -o ziploy https://raw.githubusercontent.com/code-soup/ziploy-cli/master/ziploy
 
@@ -11,4 +7,4 @@ curl -o ziploy https://raw.githubusercontent.com/code-soup/ziploy-cli/master/zip
 chmod u+x ./ziploy
 
 # Run
-./ziploy "$1" "$3" "$2"
+./ziploy "${1}" "${3}" "${2}"
